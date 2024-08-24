85be81b1c3620fdabd796ab61dad058809182d26
robinetta <alxr19@gmail.com>
2024-08-24 11:33:57 -0400

    port fix

--- index.js
+++ index.js
@@ -274,8 +274,8 @@ app.post("/submit", function (req, res) {
 
   //  });
 
-app.listen(3000, function() {
-
-    console.log("Server on Port 3000...");
-
-  });
+  const port = process.env.PORT | 3000
+  
+  app.listen(port, function() {
+      console.log(`Server on ${port}...`);
+    });
